43df6962522987b78d77e2811aeeba238e2f8ea4
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-23 12:57:00 +0100

    Update build_bridge.sh

--- build_bridge.sh
+++ build_bridge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/noetic/setup.bash
-source /opt/ros/$ROS_DISTRO/local_setup.bash
+unset $ROS_DISTRO
+source /opt/ros/$ROS1_DISTRO/setup.bash
+unset $ROS_DISTRO
+source /opt/ros/$ROS2_DISTRO/local_setup.bash
 env
 colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure

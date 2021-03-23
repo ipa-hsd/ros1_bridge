293d1aa7978579a622d6ba22970b60e17243da72
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-23 02:24:57 +0100

    Update build_bridge.sh

--- build_bridge.sh
+++ build_bridge.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
 
-echo $ROS1_DISTRO
-if [ -f /opt/ros/$ROS1_DISTRO/setup.bash ]; then
-    . /opt/ros/$ROS1_DISTRO/setup.bash
-    echo "file exists"
-else
-    echo "file does not exist"
-fi
+colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure

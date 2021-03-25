9f9929131a598a1b526ec0521f11edde7e31b97e
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-25 15:38:15 +0100

    remove dummy comment

--- build_bridge.sh
+++ build_bridge.sh
@@ -8,5 +8,4 @@ source /opt/ros/$ROS2_DISTRO/local_setup.bash
 mkdir -p ~/colcon_ws/src
 cd ~/colcon_ws/
 cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
-# dummy
 colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure

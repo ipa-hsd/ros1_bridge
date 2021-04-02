84520da99025396046cf5c002d7f3b4c4ace1dd0
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-02 12:08:03 +0200

    setup ccache

--- build_bridge.sh
+++ build_bridge.sh
@@ -5,7 +5,9 @@ unset $ROS_DISTRO
 source /opt/ros/$ROS1_DISTRO/setup.bash
 unset $ROS_DISTRO
 source /opt/ros/$ROS2_DISTRO/local_setup.bash
-# mkdir -p ~/colcon_ws/src
-# cd ~/colcon_ws/
-# cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
-colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
+mkdir -p ~/colcon_ws/src
+cd ~/colcon_ws/
+cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure --event-handlers console_direct+
